41b54637c5796d497e5bb46cc0cd3b1103177b0d
IsaacLira42 <isaaclira422@gmail.com>
2026-02-17 16:02:08 -0300

    refactor: Remove o campo timestamp da classe ApiError

--- Backend/src/main/java/com/AgendaMed/Backend/exception/ApiError.java
+++ Backend/src/main/java/com/AgendaMed/Backend/exception/ApiError.java
@@ -1,6 +1,5 @@
 package com.AgendaMed.Backend.exception;
 
-import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
@@ -8,11 +7,9 @@ public class ApiError {
 
     private final int status;
     private final String message;
-    private final LocalDateTime timestamp;
 
     public ApiError(int status, String message) {
         this.status = status;
         this.message = message;
-        this.timestamp = LocalDateTime.now();
     }
 }
