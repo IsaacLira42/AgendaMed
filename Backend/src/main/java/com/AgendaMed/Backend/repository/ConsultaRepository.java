4efcedba5c990661b305cda3eef7c4f1ead97f5a
IsaacLira42 <isaaclira422@gmail.com>
2026-02-24 20:21:09 -0300

    refactor: substitui método de busca de consultas por JPQL customizada com @Query

--- Backend/src/main/java/com/AgendaMed/Backend/repository/ConsultaRepository.java
+++ Backend/src/main/java/com/AgendaMed/Backend/repository/ConsultaRepository.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.AgendaMed.Backend.model.Consulta;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
@@ -11,7 +14,21 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
 	List<Consulta> findByMedicoIdAndDataHoraBetween(Long medicoId, LocalDateTime inicio, LocalDateTime fim);
 
-	List<Consulta> findByPacienteIdAndDataHoraAfterOrderByDataHoraAsc(
-			Long pacienteId,
-			LocalDateTime dataHora);
+	// List<Consulta> findByPacienteIdAndDataHoraAfterOrderByDataHoraAsc(
+	// Long pacienteId,
+	// LocalDateTime dataHora);
+
+	@Query("""
+			SELECT c FROM Consulta c
+			JOIN FETCH c.medico m
+			JOIN FETCH m.usuario
+			JOIN FETCH c.paciente p
+			JOIN FETCH p.usuario
+			WHERE p.id = :pacienteId
+			AND c.dataHora > :agora
+			ORDER BY c.dataHora ASC
+			""")
+	List<Consulta> buscarAgendaPaciente(
+			@Param("pacienteId") Long pacienteId,
+			@Param("agora") LocalDateTime agora);
 }
