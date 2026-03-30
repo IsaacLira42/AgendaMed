076706e79091358884920db49d794d653dddc82f
IsaacLira42 <isaaclira422@gmail.com>
2026-03-30 16:18:51 -0300

    feat: adiciona controlador AdminController para gerenciar rotas administrativas

---
+++ Backend/src/main/java/com/AgendaMed/Backend/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.AgendaMed.Backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+}
